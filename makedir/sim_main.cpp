9943165434e5699710e86f08290a3b9221575a2e
xver <victor.besyakov@gmail.com>
2019-12-02 17:30:30 -0600

    add timestamp

--- makedir/sim_main.cpp
+++ makedir/sim_main.cpp
@@ -1,13 +1,23 @@
 #include "Vshunt_verilator_dpi.h"
 #include "verilated.h"
 #include "verilated_vcd_c.h"
+#include <time.h>
+#include <sys/time.h>
+vluint64_t main_time = 0;
+struct timeval startTime;
+struct timeval currentTimeSec;
 
+double sc_time_stamp() {
+  struct timeval currentTime_func;
+  gettimeofday(&currentTime_func, NULL);
+  //return (currentTime_func.tv_sec * (int)1e6 + currentTime_func.tv_usec);
+  return (currentTime_func.tv_sec);
+}
 
 int main(int argc, char **argv, char **env) {
   int i;
   int clk_main;
   // Current simulation time (64-bit unsigned)
-  vluint64_t main_time = 0;
 
   Verilated::commandArgs(argc, argv);
   // init top verilog instance
@@ -19,11 +29,14 @@ int main(int argc, char **argv, char **env) {
   tfp->open ("top_sim.vcd");
   // initialize simulation inputs
   top_sim->clk_i = 1;
+  gettimeofday(&startTime, NULL);
   while (!Verilated::gotFinish()) { 
     top_sim->clk_i = !top_sim->clk_i;
     top_sim->eval(); 
     tfp->flush();
     tfp->dump(main_time);
+    gettimeofday(&currentTimeSec, NULL);
+    printf(">>>@%0ld sec\n",currentTimeSec.tv_sec-startTime.tv_sec);
     main_time++;  // Time passes...
   }
   // Destroy model
