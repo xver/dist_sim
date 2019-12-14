32744b81a88fff30bc6ebfc422dc87d860308693
xver <victor.besyakov@gmail.com>
2019-12-14 15:05:46 -0600

    update: benchmark

--- rtl/top_rtl.v
+++ rtl/top_rtl.v
@@ -1,18 +1,26 @@
 /* verilator lint_off UNDRIVEN */
 /* verilator lint_off UNUSED */
 /* verilator lint_off UNOPTFLAT */
-module top_rtl(input reg i_clk,input reg reset_n);
+module top_rtl(input reg i_clk,input reg reset_n,output reg benchmark_event);
   
    reg        wen    [`N_LINK:0];
    reg [31:0] token  [`N_LINK:0];
    reg [31:0] clk_cnt[`N_LINK:0];
    reg [31:0] id     [`N_LINK:0];
 
+   // 
+   initial Frng_if.frng_success = Frng_if.set_iam(`MY_TOP_NAME);
+   shunt_fringe_if Frng_if (i_clk);
+   //
+   
    assign wen    [0] = wen      [`N_LINK];
    assign token  [0] = token    [`N_LINK];
    assign clk_cnt[0] = clk_cnt  [`N_LINK];
    assign id     [0] = id       [`N_LINK];
-
+   
+   assign  benchmark_event =  wen[`N_LINK];
+   
+      
    generate
       for(genvar i=0;i<`N_LINK;i++)
         begin: chain
